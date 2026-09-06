ff1bf242dd8ae8292fe9b438ffa1f8759e6f3de0
JadAbouHawili <81527369+JadAbouHawili@users.noreply.github.com>
2026-09-07 01:17:23 +0300

    shorter

--- Mathlib/Combinatorics/SimpleGraph/Connectivity/EdgeConnectivity.lean
+++ Mathlib/Combinatorics/SimpleGraph/Connectivity/EdgeConnectivity.lean
@@ -201,8 +201,7 @@ theorem isEdgeReachable_top_iff_forall_nat :
       s.ncard = Fintype.card ↑s := this.symm
       _ = Fintype.card (Fin n) := Fintype.card_congr a
       _ = n := Fintype.card_fin n
-  have : s.encard = n := by simp [← this, s.coe_ncard_eq_encard]
-  simp [this, ENat.natCast_lt_succ]
+  simp [this, ← s.coe_ncard_eq_encard, ENat.natCast_lt_succ]
 
 theorem isEdgeConnected_top_iff_forall_nat :
     G.IsEdgeConnected ⊤ ↔ ∀ n : ℕ, G.IsEdgeConnected n := by
